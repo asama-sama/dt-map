9d3f4b39abd709971b8f1ec61dc27dd7a41c3700
Asama Qureshi <asama.qureshi@gmail.com>
2022-08-17 12:49:19 +1000

    initial commit. leaflet map over sydney

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'leaflet/dist/leaflet.css'
+import './App.css'
+import {Map} from './components/Map'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Map ></Map>
+    </div>
+  )
+}
+
+export default App
