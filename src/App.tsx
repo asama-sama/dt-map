eb2343088c9e2073e3610f9846ca10d792d22c0b
Asama Qureshi <asama.qureshi@gmail.com>
2023-01-12 15:49:23 +1100

    added banner to top

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,23 @@ import "rc-slider/assets/index.css";
 function App() {
   return (
     <div className="App">
+      <div className="banner">
+        This project is in beta. There may be issues when multiple users are
+        accessing the application.
+      </div>
       <nav className="Routes">
         <Link to="/">Single Source</Link>
         <Link to="/comparisons">Comparisons</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<CosEmissionsMap />} />
-        <Route path="/comparisons" element={<AirQualityXTrafficIncidents />} />
-      </Routes>
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <Routes>
+          <Route path="/" element={<CosEmissionsMap />} />
+          <Route
+            path="/comparisons"
+            element={<AirQualityXTrafficIncidents />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
